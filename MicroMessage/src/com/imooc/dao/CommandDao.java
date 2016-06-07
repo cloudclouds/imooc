8e56bd36d66f9f92a36b04a43c44f05c85aa000d
cloudclouds <1245848211@qq.com>
2016-06-07 22:31:47 +0800

    modify

---
+++ MicroMessage/src/com/imooc/dao/CommandDao.java
@@ -0,0 +1,37 @@
+package com.imooc.dao;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.imooc.bean.Command;
+import com.imooc.db.DBAccess;
+
+public class CommandDao {
+	/*
+	 * 根据查询条件查询指令列表
+	 */
+  public List<Command> queryCommandList(String name,String description)
+  {
+	  SqlSession sqlSession=null;
+	  DBAccess dbAccess=new DBAccess();
+	  List<Command> commandList=null;
+	  Command command=new Command();
+	  command.setName(name);
+	  command.setDescription(description);
+	  try 
+	  {
+		sqlSession= dbAccess.getSqlSession();
+		//通过sqlSession执行SQL语句
+		commandList=sqlSession.selectList("Command.queryCommand",command);
+	   } catch (IOException e) {
+		e.printStackTrace();
+	   }
+	  finally
+	  {
+		  if(sqlSession!=null)   sqlSession.close();
+	  }
+	  return commandList;
+    }
+}
--- MicroMessage/src/com/imooc/service/QueryService.java
+++ MicroMessage/src/com/imooc/service/QueryService.java
@@ -1,8 +1,12 @@
 package com.imooc.service;
 
 import java.util.List;
+import java.util.Random;
 
+import com.imooc.bean.Command;
+import com.imooc.bean.CommandContent;
 import com.imooc.bean.Message;
+import com.imooc.dao.CommandDao;
 import com.imooc.dao.MessageDao;
 import com.imooc.util.Iconst;
 
@@ -25,26 +29,32 @@ public class QueryService {
 	   * @param command 指令
 	   * @return 自动回复的内容
 	   */
-	  public String queryByCommand(String command)
+	  public String queryByCommand(String name)
 	  {
-		  MessageDao messageDao=new MessageDao();
-		  List<Message>messageList=null;
-		  if(Iconst.HELP_COMMAND.equals(command))
+//		  MessageDao messageDao=new MessageDao();
+//		  List<Message>messageList=null;
+		  CommandDao commandDao=new CommandDao();
+		  List<Command> commandList=null;
+		  if(Iconst.HELP_COMMAND.equals(name))
 	      {
-			 messageList=messageDao.queryMesageList(null, null);
+			 commandList=commandDao.queryCommandList(null,null);
 			 StringBuilder result=new StringBuilder();
-			 int size=messageList.size();
+			 int size=commandList.size();
 			 for(int i=0;i<size;i++)
 			 {
 				 if(i>0) result.append("<br/>");
-				 result.append("回复["+messageList.get(i).getCommand()+"]，可以查看"+messageList.get(i).getDescription());
+				 result.append("回复["+commandList.get(i).getName()+"]，可以查看"+commandList.get(i).getDescription());
 			 }
 			 return result.toString();
 	      }
-		  messageList=messageDao.queryMesageList(command, null);
-		  if(messageList.size()>0)
+		  commandList=commandDao.queryCommandList(name, null);
+		  if(commandList.size()>0)
 	      {
-	    	  return messageList.get(0).getContent();
+	    	  List<CommandContent> contentList=commandList.get(0).getContentList();
+	    	  int i=new Random().nextInt(contentList.size());
+	    	  System.out.println(contentList.size());
+	    	  System.out.println("i-->"+i);
+	    	  return contentList.get(i).getContent();
 	      }
 	      else return Iconst.NO_MATCHING_CONTENT;
 	  }
